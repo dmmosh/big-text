3165eee34e151cb0a7c66b4aac7a72a287926e62
wettestsock <evilsharkeater@gmail.com>
2024-01-12 12:14:53 -0500

    nothing of note

--- big_text.cpp
+++ big_text.cpp
@@ -90,7 +90,7 @@ std::string big_text(const std::string& input){
 int main(int argc, char **argv){
     
 
-    /*
+    
     if(strcmp(argv[1], "--help")) {
         std::system(("echo -e \"" + big_text("BIG ASS TEXT") + 
         "by Dmytro Moshkovskyi\n" +
@@ -98,7 +98,7 @@ int main(int argc, char **argv){
         "written in c++\n\"").c_str());
         return 0;
     }
-    */
+    
     
     // ARGV LIST IS THE PARAMTERS TO THE EXECUTABLE!!
     //std::cout << argv[1] << N;
