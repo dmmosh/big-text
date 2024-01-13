62cc2b4ad315841c809ed9978ef5c9b1284a0597
wettestsock <evilsharkeater@gmail.com>
2024-01-12 23:45:09 -0500

    nothing of note

--- serial.cpp
+++ serial.cpp
@@ -2,14 +2,27 @@
 #include <boost/archive/text_iarchive.hpp> 
 #include <fstream>
 #include <string>
+using namespace boost::archive;
 
+void save()
+{
+  std::ofstream file{"archive.txt"};
+  text_oarchive oa{file};
+  int i = 1;
+  oa << i;
+}
 
-int main() {
-    //std::ifstream txtFile("etc/commands.txt");
-    std::ofstream file{"etc/commands.txt"};
-    boost::archive::text_oarchive oa{file};
+void load()
+{
+  std::ifstream file{"archive.txt"};
+  text_iarchive ia{file};
+  int i = 0;
+  ia >> i;
+  std::cout << i << '\n';
+}
 
-    std::string stream;
-
-    oa << "fdjsl" << '\n'; 
+int main()
+{
+  save();
+  load();
 }
