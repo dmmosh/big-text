cb0f944798d866783bb2dcf9a0f5d70822ca8c44
wettestsock <evilsharkeater@gmail.com>
2024-01-12 23:46:07 -0500

    nothing of note

--- serial.cpp
+++ serial.cpp
@@ -1,22 +1,22 @@
-#include <boost/archive/text_oarchive.hpp> //serialization
-#include <boost/archive/text_iarchive.hpp> 
-#include <fstream>
-#include <string>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 #include <iostream>
+#include <sstream>
+
 using namespace boost::archive;
+
+std::stringstream ss;
 
 void save()
 {
-  std::ofstream file{"archive.txt"};
-  text_oarchive oa{file};
+  text_oarchive oa{ss};
   int i = 1;
   oa << i;
 }
 
 void load()
 {
-  std::ifstream file{"archive.txt"};
-  text_iarchive ia{file};
+  text_iarchive ia{ss};
   int i = 0;
   ia >> i;
   std::cout << i << '\n';
