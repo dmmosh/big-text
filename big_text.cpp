9922720e3e7dd4ac0f155a360bc92ae651253902
wettestsock <evilsharkeater@gmail.com>
2024-01-12 22:40:35 -0500

    nothing of note

--- big_text.cpp
+++ big_text.cpp
@@ -142,8 +142,8 @@ int main(int argc, char** argv){
 
         continue;
         // --times=
-        } else if (!strncmp(argv[i], "--times=", 6)) {
-        if(!is_int(argv[i], 6)) {
+        } else if (!strncmp(argv[i], "--times=", 8)) {
+        if(!is_int(argv[i], 8)) {
             std::system("echo -e \"ERROR: can't multiply, not a number specified\"");
             return 0;
         }
