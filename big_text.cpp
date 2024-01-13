df7e2b504e0a457a477fdc450773ace4e9f87754
wettestsock <evilsharkeater@gmail.com>
2024-01-12 20:39:36 -0500

    nothing of note

--- big_text.cpp
+++ big_text.cpp
@@ -102,7 +102,7 @@ int main(int argc, char** argv){
 
         //if first element or has newline 
         //makes a new element and iterates to it
-        if (!strcmp(argv[i], "n")) {
+        if (std::string(argv[i]).find('\n') != std::string::npos) {
             out.push_back("");
             continue;
         }
