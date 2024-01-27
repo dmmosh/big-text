d4ff742a30559418607b7bc658c843e33bd0eed6
wettestsock <evilsharkeater@gmail.com>
2024-01-27 14:58:41 -0500

    nothing of note

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read -p "What does above text say?  " check
 
 if [ ! "${check^^}" == "PINEAPPLE" ] 
 then
-    echo -e "\nCan't have cool text if you can't see it.\nChances are your terminal is not currently enabled with UTF-8 character encoding.\n"
+    echo -e "\nCan't have cool text if you can't see it.\nChances are your terminal is not currently enabled with UTF-8 character encoding.\nOr you had a typo."
     exit 1
 fi
 
