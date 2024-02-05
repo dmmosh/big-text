3b5c6b18c21e322454e09e478dcf6adccfa753de
wettestsock <evilsharkeater@gmail.com>
2024-02-05 13:20:48 -0500

    hello

--- utils.cpp
+++ utils.cpp
@@ -89,7 +89,7 @@ std::string big_text(const std::string& input, const bool& have_lines){
 
     //adds the lines
     if(have_lines){
-        for(size_t i=0; i<char_str-1; i++){
+        for(size_t i=0; i<char_str; i++){
             top_line += "▀";
             bottom_line += "▄";
         }
