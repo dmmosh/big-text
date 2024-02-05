8436776bebeb8d580e7498a5d3b3c5d00889be5d
wettestsock <evilsharkeater@gmail.com>
2024-02-05 12:50:01 -0500

    hello

--- utils.cpp
+++ utils.cpp
@@ -4,11 +4,10 @@
 //converts to BIG text
 //string as value because it might be modified
 std::string big_text(const std::string& input, const bool& have_lines){
-    static int char_ctr; //counter of characters 
+    static int char_ctr =0; //counter of characters 
     int t_cols = std::stoi(exec("stty size | awk '{print $2}'"));
 
-    std::cout << char_ctr << N << t_cols << N;
-    if(!input.length() || char_ctr > t_cols){
+    if(!input.length()){
         char_ctr = 0;
         return "\n";
     }
@@ -81,6 +80,11 @@ std::string big_text(const std::string& input, const bool& have_lines){
 
     top.resize(top.size() - 1);
     bottom.resize(bottom.size() - 1);
+
+    if(char_ctr > t_cols){
+        char_ctr = 0;
+        return "\n";
+    }
 
 
     //returns the fat string
