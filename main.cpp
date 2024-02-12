cffdef51842e6cc87810bfe6853f24e7a7db34bb
wettestsock <evilsharkeater@gmail.com>
2024-02-11 22:31:00 -0500

    nothing of note

--- main.cpp
+++ main.cpp
@@ -13,17 +13,9 @@ dsj
 
 
 int main(int argc, char** argv){
-    static std::vector<std::string> arg_input;
-    for (size_t i = 0; i < argc; i++)
-    {
-        arg_input.push_back(argv[i]);
-    }
-    
-    
-    
     
     //gets the path of executable 
-    static std::string path = arg_input[0];
+    static std::string path = argv[0];
 
     for(int i=path.length(); i>=0; i--){
         if(path[i] == '/'){
@@ -39,49 +31,49 @@ int main(int argc, char** argv){
     }
     
 
-    std::vector<std::string> out = {""}; //output words, not big yet
+    std::string out; //output words, not big yet
     bool line = 1; //whether to have a line or not, default 1
     int repeat = 1; //whether to repeat or notfdsf
 
     for(int i=1; i<argc; i++){ //iterates over the argument list
+        std::string arg_input = argv[i]; //converts to string
 
         // -h, --help
-        if(arg_input[i] == "--help" || arg_input[i] == "-h") {
+        if(arg_input == "--help" || arg_input == "-h") {
             help(path);
             return 0;
         // -n, --newline
-        } else if(arg_input[i] == "--newline" || arg_input[i] == "-n") {
-            if(out.back().length() != 0){
-                out.back().resize(out.back().size() -1);
+        } else if(arg_input == "--newline" || arg_input == "-n") {
+            if(out.length() != 0){
+                out.resize(out.size() -1);
             };
-            out.push_back(""); //pushes a new element
+            out += '\n';
         // -nl, --no-lines
-        } else if(arg_input[i] == "--no-lines" || arg_input[i] == "-nl") {
+        } else if(arg_input == "--no-lines" || arg_input == "-nl") {
             line = 0; //removes lines
         // -x=
-        } else if(arg_input[i].substr(0,3) == "-x=") {
-            if(!is_int(arg_input[i], 3)) {
+        } else if(arg_input.substr(0,3) == "-x=") {
+            if(!is_int(arg_input, 3)) {
                 sys("ERROR: can't multiply, not a number specified");
                 return 0;
             }
-            repeat = std::stoi(arg_input[i].substr(3)); //sets to the repeat value
+            repeat = std::stoi(arg_input.substr(3)); //sets to the repeat value
         // --times=
-        } else if (arg_input[i].substr(0,8) == "--times=") {
-            if(!is_int(arg_input[i], 9)) {
+        } else if (arg_input.substr(0,8) == "--times=") {
+            if(!is_int(arg_input, 9)) {
                 sys("ERROR: can't multiply, not a number specified");
                 return 0;
             }
-            repeat = std::stoi(arg_input[i].substr(9)); //sets to the repeat value
+            repeat = std::stoi(arg_input.substr(9)); //sets to the repeat value
         } else {
 
         //if first element or has newline 
         //makes a new element and iterates to it fdg
     
-        out.push_back(arg_input[i] + " ");
+        
         }
     }
 
-    
     //remove space from last element
     if(out.back().length() != 0){
         out.back().resize(out.back().size() -1);
