85e84b363523a0fe98deb7511e1068dd246b6aa2
wettestsock <evilsharkeater@gmail.com>
2024-02-12 00:24:51 -0500

    nothing of note

--- main.cpp
+++ main.cpp
@@ -75,8 +75,8 @@ int main(int argc, char** argv){
     //iterate over characters in the output string
     int start_i = 0; //index the last space is in (default 0)
     for(const char& c: out){
-        if(c == '\n' || big_out.back().char_str >= t_cols -6){
-            std::cout << t_cols-6 << N << big_out.back().char_str << N ;
+        if(c == '\n' || big_out.back().char_str >= t_cols -5){
+            std::cout << t_cols-5 << N << big_out.back().char_str << N ;
             big_out.push_back(btxt(line));
         } else {
             big_out.back() += c;
